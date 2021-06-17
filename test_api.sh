227377ce26f278ac86de8e49a29a4467fec86e70
EC2 Default User <ec2-user@ip-172-16-60-0.ec2.internal>
2021-06-17 07:28:14 +0000

    add deployment

--- test_api.sh
+++ test_api.sh
@@ -1,1 +1,1 @@
-curl -F "file=@./data/images/inference/bus.jpg; filename='bus.jpg'" https://pny715mt77.execute-api.us-east-1.amazonaws.com/yolov5 
+curl -F "file=@./data/images/inference/bus.jpg; filename='bus.jpg'" https://XXX.execute-api.us-east-1.amazonaws.com/yolov5 
