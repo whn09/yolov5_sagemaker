ce57c6fc93b5a65f11ac29fc9d7e7736a2353291
EC2 Default User <ec2-user@ip-172-16-43-25.ec2.internal>
2021-06-27 04:29:13 +0000

    add tensorboard support

--- train_local.sh
+++ train_local.sh
@@ -2,5 +2,5 @@
 
 image=$1
 
-nvidia-docker run -v $(pwd)/data:/opt/ml/input/data --shm-size=8g --rm ${image} train
-# docker run -v $(pwd)/data:/opt/ml/input/data --shm-size=8g --rm ${image} train
+nvidia-docker run -v $(pwd)/data:/opt/ml/input/data -p 6006:6006 --shm-size=8g --rm ${image} train
+# docker run -v $(pwd)/data:/opt/ml/input/data -p 6006:6006 --shm-size=8g --rm ${image} train
